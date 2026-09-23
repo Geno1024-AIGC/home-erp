3aedbb742748593d080444f8c9c5003c6dea39e0
Y. Z. Chen <754097987@qq.com>
2026-09-23 21:22:14 +0800

    {Add} project: Bootstrap Gradle KTS multi-module build with 9.7.1 wrapper.

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    includeBuild("build-logic")
+}
+
+rootProject.name = "home-erp"
+
+include("swrepo:spi")
+include("swrepo:members")
+include("swrepo:inventory")
+include("swrepo:finances")
+include("swrepo:chores")
+include("star")
